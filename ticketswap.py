e8baf40d78870fa1dd142d6fac5c72aceab45c5f
James Lynn <j.lynn3237@gmail.com>
2016-09-28 14:06:22 -0400

    Finished

--- ticketswap.py
+++ ticketswap.py
@@ -1,17 +1,22 @@
-k = 0
-global solutions
-solutions = []
+import sys
+import itertools
 
 class Passenger:
     pass
 
+
+class Subway:
+    pass
+
+global solutions
+solutions = []
 passengers = list()
+subway = Subway()
 
-
-# open and parse file
-def openFile():
+# open a file and make passengers
+def openFile(fileName):
     # open input file and parse the data
-    r = open("tickettest.txt", "r")
+    r = open(fileName, "r")
     for line in r:
         array = line.split(".")
         # go through the array and get the passengers
@@ -38,51 +43,95 @@ def makePassengerFromString(string):
 
     # construct passenger data
     temp.name = array[0]
-    temp.entryStation = int(array[1])
+    temp.startStation = int(array[1])
     temp.exitStation = int(array[2])
 
     # add them to the list
     passengers.append(temp)
 
 
-def construct_candidates(a, k):
-    candidates = []
-    return candidates
+def tick():
+    subway.currentStation += 1
+
+    # add the people that get on at station one
+    for i in range(0, len(passengers)):
+        if passengers[i].startStation is subway.currentStation:
+            subway.passengers.append(passengers[i])
 
 
-# def make_move():
-
-
-# def unmake_move():
-
-
-#def is_a_solution():
-
-
-def backtrack(a, k, profitLoss):
-    if is_a_solution(a, k):
-        if profitLoss not in solutions:
-            solutions.append(profitLoss)
+def createPrice(x):
+    price = 0
+    if x is 0:
+        return price
+    elif x is 1:
+        return 1.0
+    elif x is 2:
+        return 1.9
+    elif x is 3:
+        return 2.7
+    elif x is 4:
+        return 3.4
+    elif x is 5:
+        return 4.0
+    elif x is 6:
+        return 4.5
+    elif x is 7:
+        return 4.9
+    elif x is 8:
+        return 5.2
+    elif x is 9:
+        return 5.4
+    elif x is 10:
+        return 5.5
     else:
-        k += 1
-        c = construct_candidates(a, k)
-        for i in range(0, len(c)):
-            a.insert(k, c[i])
-            #make_move()
-            backtrack(a, k, profitLoss)
-            #unmake_move()
+        return 5.5
 
 
+def calculateTotalCost():
+    totalCost = 0
+    for i in range(0, len(subway.passengers)):
+        stationsTraveled = subway.passengers[i].exitStation - subway.passengers[i].startStation
+        price = createPrice(stationsTraveled)
+        totalCost += price
+        round(totalCost, 1)
 
-def main():
-    openFile()
-
-    a = []
-    k = 0
-    profitLoss = 0
-
-    backtrack(a, k, profitLoss)
+    return float(format(totalCost, '.1f'))
 
 
+def main(fileName):
+    openFile(fileName)
 
-main()
+    startList = []
+    exitList = []
+    priceList = []
+
+    for i in range(0, len(passengers)):
+        exitList.append(passengers[i].exitStation)
+
+    for i in range(0, len(passengers)):
+        startList.append(passengers[i].startStation)
+
+    subway.startStation = min(startList)
+    subway.currentStation = 0
+    subway.endStation = max(exitList)
+    subway.passengers = []
+
+    # put all the passengers on the train and the train at the end
+    for i in range(subway.startStation, subway.endStation+1):
+        tick()
+
+    totalCost = calculateTotalCost()
+
+    perms = list(itertools.permutations(startList))
+
+    for i in range(0, len(perms)):
+        for j in range(0, len(subway.passengers)):
+            subway.passengers[j].startStation = perms[i][j]
+        priceList.append(calculateTotalCost())
+
+    for i in range(0, len(priceList)):
+        priceList[i] = totalCost - priceList[i]
+
+    print("loss(", float(format((max(priceList)), '.1f')), ").")
+
+main(sys.argv[1])
