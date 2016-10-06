387b921a1ba7dffe3304af9fb39084e2e88ae1d4
James Lynn <j.lynn3237@gmail.com>
2016-10-06 17:47:11 -0400

    Revamping

--- elevators.py
+++ elevators.py
@@ -1,6 +1,7 @@
-import sys
+import copy, sys
 
 elevators = list()
+elevators2 = list()
 highestFloor = 0
 numElevators = 0
 k = 0
@@ -19,9 +20,11 @@ peter = Peter()
 
 
 # open and parse file
-def open_file(fileName):
+def open_file():
+    global elevators
+    elevators = []
     # open input file and parse the data
-    r = open(fileName, "r")
+    r = open("elevatortest.txt", "r")
     for line in r:
         array = line.split(".")
         # go through the array and get the highest floor
@@ -70,53 +73,60 @@ def construct_candidates(a, k):
     # find an elevator that is at peter's floor
     for i in range(0, len(elevators)):
         # if the elevator's top or bottom is where peter currently is
-        if (a[k-1].endFloor is elevators[i].endFloor) | (a[k-1].endFloor is elevators[i].startFloor):
+        if (a[k-1].currentFloor is elevators[i].endFloor) | (a[k-1].currentFloor is elevators[i].startFloor):
             if a[k-1].elevatorNum is not elevators[i].elevatorNum:
-                candidates.append(elevators[i])
-
+                if elevators[i] not in a:
+                    candidates.append(elevators[i])
     return candidates
 
 
-def move_elevators():
-    for i in range(0, len(elevators)):
-        if elevators[i].currentFloor is elevators[i].endFloor:
-            elevators[i].direction = 0
-        elif elevators[i].currentFloor is elevators[i].startFloor:
-            elevators[i].direction = 1
+def move_elevators(x):
+    for j in range(x):
+        for i in range(0, len(elevators)):
+            if elevators[i].currentFloor is elevators[i].endFloor:
+                elevators[i].direction = 0
+            elif elevators[i].currentFloor is elevators[i].startFloor:
+                elevators[i].direction = 1
 
-        if elevators[i].direction is 1:
-            elevators[i].currentFloor += 1
-        elif elevators[i].direction is 0:
-            elevators[i].currentFloor -= 1
+            if elevators[i].direction is 1:
+                elevators[i].currentFloor += 1
+            elif elevators[i].direction is 0:
+                elevators[i].currentFloor -= 1
 
 
 def make_move(a, k, currentFloor):
     timetaken = 0
     # waiting for the elevator
     while a[k].currentFloor is not currentFloor:
-        move_elevators()
+        move_elevators(1)
         timetaken += 1
 
-    # move elevator to the top
-    x = a[k].endFloor - a[k].currentFloor
+    # move elevator to the top or bottom
+    if a[k].currentFloor is not highestFloor:
+        if a[k].currentFloor is a[k].endFloor:
+            x = a[k].currentFloor - a[k].startFloor
+        else:
+            x = a[k].endFloor - a[k].currentFloor
 
-    for i in range(x):
-        move_elevators()
-        timetaken += 1
+    move_elevators(x)
+    timetaken += x
 
     return timetaken
 
 
-def unmake_move(timetaken):
-    for i in range(0, timetaken):
-        move_elevators()
+def unmake_move(a, k, currentFloor, prevElevators):
+    while a[k-1].currentFloor is not currentFloor:
+        move_elevators(1)
+
     for i in range(0, len(elevators)):
         x = elevators[i].endFloor - elevators[i].startFloor
-        if x % 2 is 1:
+        if x % 2 is 0:
             if elevators[i].direction is 1:
                 elevators[i].direction = 0
             else:
                 elevators[i].direction = 1
+    # for i in range(0, len(elevators)):
+    #     elevators[i] = copy.deepcopy(prevElevators[i])
 
 
 def is_a_solution(a, k):
@@ -130,6 +140,9 @@ def backtrack(a, k, timepassed):
     if is_a_solution(a, k):
         if timepassed not in solutions:
             solutions.append(timepassed)
+            for i in range(0, len(a)):
+                print(a[i].elevatorNum)
+            print("Finish time: ", timepassed)
     # no? then get on another elevator
     else:
         k += 1
@@ -137,16 +150,17 @@ def backtrack(a, k, timepassed):
         for i in range(0, len(c)):
             a.insert(k, c[i])
             currentFloor = a[k-1].currentFloor
+            prevElevators = copy.deepcopy(elevators)
             timetaken = make_move(a, k, currentFloor)
             timepassed += timetaken
             backtrack(a, k, timepassed)
-            unmake_move(timetaken)
+            unmake_move(a, k, currentFloor, prevElevators)
             a.pop(k)
             timepassed -= timetaken
 
 
-def main(fileName):
-    open_file(fileName)
+def main():
+    open_file()
     # initial set up
     # peter on 0th floor, no time passed
     peter.currentFloor = 0
@@ -161,14 +175,18 @@ def main(fileName):
         if elevators[i].startFloor is 0:
             a.insert(0, elevators[i])
             x = a[0].endFloor - a[0].startFloor
-            for i in range(x):
-                move_elevators()
-                timepassed += 1
+            move_elevators(x)
+            timepassed += x
             # start the backtracking
             backtrack(a, k, timepassed)
+            # reset
+            timepassed = 0
+            open_file()
+            k = 0
+            a = []
 
     global solutions
     print("min_time(", min(solutions), ").")
 
 
-main(sys.argv[1])
+main()
